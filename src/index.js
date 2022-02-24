7dd5b40296934a935fe02134366ed56b9adcf7ef
ScaronTr <forever2652453@gmail.com>
2022-02-24 18:18:54 +0300

    feat: create check functioin

--- src/index.js
+++ src/index.js
@@ -1,3 +1,56 @@
+let arr;
+let storage;
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  arr = [];
+  storage = [];
+  const pairs = {
+    '(': ')',
+    '{': '}',
+    '[': ']',
+    '|': '|',
+    '1': '2',
+    '3': '4',
+    '5': '6',
+    '7': '7',
+    '8': '8',
+  };
+
+  arr = str.split('');
+
+  for (let item of arr) {
+     for (let key in pairs) {
+        if (item === key && item === pairs[key]) {
+           if (storage.length === 0) {
+              storage.push(item);
+              break;
+           } else {
+              if (key !== storage[storage.length - 1]) {
+                 storage.push(item);
+                 break;
+              } else if (key === storage[storage.length - 1]) {
+                 storage.pop();
+                 break;
+              } else {
+                 return false;
+              }
+           }
+        } else if (item === key && item !== pairs[key]) {
+           storage.push(item);
+           break;
+        } else if (item === pairs[key]) {
+           if (storage.length !== 0) {
+              if (key === storage[storage.length - 1]) {
+                 storage.pop();
+                 break;
+              } else {
+                 return false;
+              } 
+           } else {
+              return false;
+           };
+        };
+     };
+  };
+  return storage.length === 0 ? true : false;
 }
